0627c6fcbfb9085408981aa25755285ff018fae1
Alexis <alexis.blancfeuillet@gmail.com>
2024-02-06 22:24:46 +0100

    Adding buttons
    I added 1 activity and 3 buttons.
    One on the main activity to access the second.
    Two on the second activity to switch previous and next.

--- AlexisSimon/app/src/main/java/es/upm/alexissimon/MainActivity.kt
+++ AlexisSimon/app/src/main/java/es/upm/alexissimon/MainActivity.kt
@@ -1,10 +1,18 @@
 package es.upm.alexissimon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val buttonNextActivity: Button = findViewById(R.id.goto_second_activity)
+        buttonNextActivity.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
