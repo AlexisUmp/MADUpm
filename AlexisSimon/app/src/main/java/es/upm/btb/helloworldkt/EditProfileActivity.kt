6b7a52b5d68f1ce873d80c5e60f2521cb877e1e5
Sim <simon.bellegy@epita.fr>
2024-02-24 17:25:52 +0100

    tried creating passwords and encrypt/decrypt

--- AlexisSimon/app/src/main/java/es/upm/btb/helloworldkt/EditProfileActivity.kt
+++ AlexisSimon/app/src/main/java/es/upm/btb/helloworldkt/EditProfileActivity.kt
@@ -6,11 +6,30 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 class EditProfileActivity : AppCompatActivity() {
+    private fun String.encrypt(password: String): String {
+        val secretKeySpec = SecretKeySpec(password.toByteArray(), "AES")
+        val iv = ByteArray(16)
+        val charArray = password.toCharArray()
+        for (i in 0 until charArray.size){
+            iv[i] = charArray[i].toByte()
+        }
+        val ivParameterSpec = IvParameterSpec(iv)
+
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec)
+
+        val encryptedValue = cipher.doFinal(this.toByteArray())
+        return Base64.encodeToString(encryptedValue, Base64.DEFAULT)
+    }
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +40,28 @@ class EditProfileActivity : AppCompatActivity() {
 
         val nameEditText = findViewById<EditText>(R.id.editTextName)
         val emailEditText = findViewById<EditText>(R.id.editTextEmail)
-
-        // Only set the initial text if it's not already set
-        if (nameEditText.text.isNullOrBlank()) {
-            nameEditText.setText(currentName)
-        }
-        if (emailEditText.text.isNullOrBlank()) {
-            emailEditText.setText(currentEmail)
-        }
+        val passwordEditText = findViewById<EditText>(R.id.editTextPassword)
+        nameEditText.setText(currentName)
+        emailEditText.setText(currentEmail)
 
         findViewById<Button>(R.id.buttonSave).setOnClickListener {
+            if (nameEditText.text.isNullOrBlank()) {
+                nameEditText.setText(currentName)
+            }
+            if (emailEditText.text.isNullOrBlank()) {
+                emailEditText.setText(currentEmail)
+            }
+            if (passwordEditText.text.isNullOrBlank()) {
+                passwordEditText.setText(currentName)
+            }
             val newName = nameEditText.text.toString()
             val newEmail = emailEditText.text.toString()
+            val newPassword = passwordEditText.text.toString()
             val sr: SharedPreferences = getSharedPreferences("label", 0)
             val editor = sr.edit()
             newName.let { editor.putString("name", it) }
             newEmail.let { editor.putString("email", it) }
+            newPassword.let {editor.putString("password", it.encrypt(it))}
             editor.apply()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
--- AlexisSimon/app/src/main/java/es/upm/btb/helloworldkt/SecondActivity.kt
+++ AlexisSimon/app/src/main/java/es/upm/btb/helloworldkt/SecondActivity.kt
@@ -8,16 +8,82 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.app.Activity
+import android.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class SecondActivity : AppCompatActivity() {
     private val TAG = "btaSecondActivity"
 
-    data class User(val name: String?, val email: String?, val score: Int)
+
+    private fun String.encrypt(password: String): String {
+        val secretKeySpec = SecretKeySpec(password.toByteArray(), "AES")
+        val iv = ByteArray(16)
+        val charArray = password.toCharArray()
+        for (i in 0 until charArray.size){
+            iv[i] = charArray[i].toByte()
+        }
+        val ivParameterSpec = IvParameterSpec(iv)
+
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec)
+
+        val encryptedValue = cipher.doFinal(this.toByteArray())
+        return Base64.encodeToString(encryptedValue, Base64.DEFAULT)
+    }
+    private fun askForUserIdentifier(sr : SharedPreferences) {
+        val input = EditText(this)
+        val passwordInput = EditText(this)
+        AlertDialog.Builder(this)
+            .setTitle("Enter User Identifier")
+            .setIcon(R.mipmap.ic_launcher)
+            .setView(input)
+            .setView(input)
+            .setPositiveButton("Save") { dialog, which ->
+                val userInput = input.text.toString()
+                val password = passwordInput.text.toString()
+                if (userInput.isNotBlank() && password.isNotBlank() && userInput == sr.getString("name", "") && password.encrypt(password) == sr.getString("password", "")) {
+                    Toast.makeText(this, "Successfully registered.", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Wrong Information.", Toast.LENGTH_LONG).show()
+                    askForUserIdentifier(sr)
+                }
+            }
+            .setNegativeButton("Register ?") { dialog, which ->
+                val intent = Intent(this, ThirdActivity::class.java)
+                startActivity(intent)
+            }
+            .show()
+    }
+
+
+    private fun String.decrypt(password: String): String {
+        val secretKeySpec = SecretKeySpec(password.toByteArray(), "AES")
+        val iv = ByteArray(16)
+        val charArray = password.toCharArray()
+        for (i in 0 until charArray.size){
+            iv[i] = charArray[i].toByte()
+        }
+        val ivParameterSpec = IvParameterSpec(iv)
+
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec)
+
+        val decryptedByteValue = cipher.doFinal(Base64.decode(this, Base64.DEFAULT))
+        return String(decryptedByteValue)
+    }
+
+    data class User(val name: String?, val email: String?, val score: Int, val password: String)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         var sr : SharedPreferences = getSharedPreferences("label", 0)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        askForUserIdentifier(sr)
         val user = getUserData(sr)
         findViewById<TextView>(R.id.textViewName).text = user.name
         findViewById<TextView>(R.id.textViewEmail).text = user.email
@@ -44,7 +110,11 @@ class SecondActivity : AppCompatActivity() {
         val score = sr.getInt("score", 0)
         val name = sr.getString("name", "User")
         val email = sr.getString("email", "No email provided")
-        return User(name, email, score)
+        val password = sr.getString("password", "").toString()
+        return if (password != "")
+            User(name, email, score, password.decrypt(password))
+        else
+            User(name, email, score, "")
     }
     /*
     override fun onCreate(savedInstanceState: Bundle?) {
